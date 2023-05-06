d543f5b90401bbf59d1bb8a1e82788fd2b4cff47
adrianlam15 <93681064+adrianlam15@users.noreply.github.com>
2023-05-06 01:38:48 -0400

    Added helper functions and configured backend

--- backend/pdfHelper.py
+++ backend/pdfHelper.py
@@ -3,19 +3,24 @@ from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
 from pdfminer.pdfpage import PDFPage
+from io import BytesIO
 
 
-def extract_text(pdf_path):
+def extract_text(pdf_file):
     resource_manager = PDFResourceManager()
     output_string = StringIO()
     laparams = LAParams()
     device = TextConverter(resource_manager, output_string, laparams=laparams)
-    fp = open(pdf_path, "rb")
     interpreter = PDFPageInterpreter(resource_manager, device)
-    for page in PDFPage.get_pages(fp):
+
+    # create a BytesIO object to read the file
+    pdf_data = BytesIO(pdf_file.read())
+
+    for page in PDFPage.get_pages(pdf_data):
         interpreter.process_page(page)
-    fp.close()
+
     device.close()
     text = output_string.getvalue()
     output_string.close()
+
     return text
