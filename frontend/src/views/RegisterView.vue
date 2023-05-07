c36b85043a0c1cf9d9e57607e057c8d5121ffc50
adrianlam15 <93681064+adrianlam15@users.noreply.github.com>
2023-05-07 18:35:34 -0400

    Added password validation (retyping)

--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -35,7 +35,28 @@
           </div>
   
           <div>
-            <button @click.prevent="submitForm" type="submit" class="hover:-translate-y-1 transition ease-in-out flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Register</button>
+            <button @click.prevent="submitForm" type="submit" class="hover:-translate-y-1 transition ease-in-out flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"><p v-if="!isLoading">Register</p>
+              <svg
+                v-if="isLoading"
+                class="animate-spin h-5 w-5 text-white"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  class="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  stroke-width="4"
+                ></circle>
+                <path
+                  class="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                ></path>
+              </svg></button>
           </div>
         </form>
         </div>
@@ -49,11 +70,13 @@
         email: '',
         password: '',
         retypePassword: '',
-        error: false
+        error: false,
+        isLoading: false
       }
     },
     methods: {
       submitForm() {
+        // add email validation
         if (this.password !== this.retypePassword) {
           this.error = true
           alert('Passwords do not match')
